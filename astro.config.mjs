f39d93a598d618b3b8860aa819e594b30d123a4c
Damian Soto <soto.platero@gmail.com>
2024-09-28 10:46:02 -0400

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+
+import svelte from '@astrojs/svelte';
+
+import tailwind from '@astrojs/tailwind';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte(), tailwind(), sitemap()]
+});
